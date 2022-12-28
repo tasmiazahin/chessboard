24ee4f23e9c78c82db1d7bb68d4176e20c9a08d5
Tasmia Zahin <tasmiamaithily@gmail.com>
2022-12-28 22:55:13 +0100

    First commit

---
+++ chessboard/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+using System.Text;
+
+namespace chessboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.OutputEncoding = Encoding.UTF8;
+            string text = Console.ReadLine();
+            int userInput = Convert.ToInt32(text);
+
+
+            for (int i = 0; i < userInput; i++)
+            {
+                for (int j = 0; j < userInput; j++)
+                {
+                    //Console.Write("◼︎");
+                    Console.Write('\u2B1B');
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
